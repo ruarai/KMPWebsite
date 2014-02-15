7e3a0854c3ae994938c22942bbc32d1090c69229
ruarai <ruarai.big@gmail.com>
2014-02-15 11:54:21 +1100

    visual studio missed a file

---
+++ KMPRazorMix/backend/GeoData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KMPRazorMix
+{
+    public class GeoData
+    {
+        public string country_code;
+
+        public double latitude;
+        public double longitude;
+    }
+}
